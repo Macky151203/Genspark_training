4d785a355fc9755cd0115c0b7493a69881f45f7f
Macky151203 <subramaniyansrinivasan2003@gmail.com>
2025-05-31 22:24:11 +0530

    changed schema

---
+++ 30-5-2025_day20/bank/Migrations/20250531154903_model_change1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bank.Migrations
+{
+    /// <inheritdoc />
+    public partial class model_change1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
