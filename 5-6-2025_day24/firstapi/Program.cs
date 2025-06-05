caf7e1e125e05c3a03882bda9687f5c666c23c88
Macky151203 <subramaniyansrinivasan2003@gmail.com>
2025-06-05 10:17:54 +0530

    added file crud

---
+++ 5-6-2025_day24/firstapi/Program.cs
@@ -0,0 +1,112 @@
+// using FirstApi.Services;
+using FirstApi.Repositories;
+using FirstApi.Contexts;
+using FirstApi.Interfaces;
+using FirstApi.Models;
+using FirstApi.Services;
+using Microsoft.EntityFrameworkCore;
+using FirstApi.Misc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using AspNet.Security.OAuth.GitHub;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.WriteIndented = true;
+    }); ;
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<CustomExceptionFilter>();
+});
+// builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+// builder.Services.AddScoped<DoctorService>();
+// builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+// builder.Services.AddScoped<PatientService>();
+// builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+// builder.Services.AddScoped<AppointmentService>();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddTransient<IRepository<int, Doctor>, DoctorRepository>();
+builder.Services.AddTransient<IRepository<int, Patient>, PatientRepository>();
+builder.Services.AddTransient<IRepository<int, Speciality>, SpecialityRepository>();
+builder.Services.AddTransient<IRepository<string, Appointment>, AppointmentRepository>();
+builder.Services.AddTransient<IRepository<int, DoctorSpeciality>, DoctorSpecialityRepository>();
+builder.Services.AddTransient<IRepository<string, User>, UserRepository>();
+
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<IOtherContextFunctionities, OtherFuncinalitiesImplementation>();
+builder.Services.AddTransient<IEncryptionService, EncryptionService>();
+builder.Services.AddTransient<ITokenService, TokenService>();
+builder.Services.AddTransient<IAuthenticationService, AuthenticationService>();
+builder.Services.AddTransient<IAppointmentService, AppointmentService>();
+builder.Services.AddTransient<IFileService, FileService>();
+
+
+
+
+
+
+
+
+//jwt auth
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//                 .AddJwtBearer(options =>
+//                 {
+//                     options.TokenValidationParameters = new TokenValidationParameters
+//                     {
+//                         ValidateAudience = false,
+//                         ValidateIssuer = false,
+//                         ValidateLifetime = true,
+//                         ValidateIssuerSigningKey = true,
+//                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Keys:JwtTokenKey"]))
+//                     };
+//                 });
+
+// builder.Services.AddAuthorization(options =>
+// {
+//     options.AddPolicy("ExperiencedDoctorOnly", policy =>
+//         policy.Requirements.Add(new ExperiencedDoctorRequirement(3)));
+// });
+
+
+builder.Services.AddScoped<IAuthorizationHandler, ExperiencedDoctorHandler>();
+
+builder.Services.AddAutoMapper(typeof(User));
+
+builder.Services.AddDbContext<ClinicContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers();
+app.Run();
+
+
