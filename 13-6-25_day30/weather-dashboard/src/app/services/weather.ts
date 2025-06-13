95df67338b8d699e9a0e3b524030540d8b39c51e
Macky151203 <subramaniyansrinivasan2003@gmail.com>
2025-06-13 19:44:17 +0530

    added weatherapi

---
+++ 13-6-25_day30/weather-dashboard/src/app/services/weather.ts
@@ -0,0 +1,32 @@
+// src/app/services/weather.service.ts
+import { Injectable, signal, effect } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class WeatherService {
+  private readonly API_KEY = 'api_key_goes_here';
+  private readonly BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+
+  weather = signal<any | null>(null);
+  error = signal<string>('');
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  fetchWeather(city: string): void {
+    const url = `${this.BASE_URL}?q=${city}&appid=${this.API_KEY}&units=metric`;
+    this.http.get<any>(url).subscribe({
+      next: data => {
+        this.weather.set(data);
+        this.error.set('');
+      },
+      error: err => {
+        console.error('Error fetching weather:', err);
+        this.weather.set(null);
+        this.error.set('Error fetching data');
+      }
+    });
+  }
+}
